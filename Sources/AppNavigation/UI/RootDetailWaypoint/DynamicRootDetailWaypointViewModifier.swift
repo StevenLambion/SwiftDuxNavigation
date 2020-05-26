aa5c26452ce51fd2920488a7ec202c8c210c8e3b
Steven Lambion <steven.lambion@atsid.com>
2020-05-25 20:45:00 -0700

    Allow access to detailItem for any platform.

--- Sources/AppNavigation/UI/RootDetailWaypoint/DynamicRootDetailWaypointViewModifier.swift
+++ Sources/AppNavigation/UI/RootDetailWaypoint/DynamicRootDetailWaypointViewModifier.swift
@@ -1,44 +1,40 @@
-#if canImport(UIKit)
+import SwiftDux
+import SwiftUI
 
-  import SwiftDux
-  import SwiftUI
+internal struct DynamicRootDetailWaypointViewModifier<T, DetailContent>: WaypointResolverViewModifier
+where T: LosslessStringConvertible & Equatable, DetailContent: View {
+  @Environment(\.rootDetailWaypointContent) private var rootDetailWaypointContent
 
-  internal struct DynamicRootDetailWaypointViewModifier<T, DetailContent>: WaypointResolverViewModifier
-  where T: LosslessStringConvertible & Equatable, DetailContent: View {
-    @Environment(\.rootDetailWaypointContent) private var rootDetailWaypointContent
+  static var hasPathParameter: Bool { true }
+  var isDetail: Bool? { true }
 
-    static var hasPathParameter: Bool { true }
-    var isDetail: Bool? { true }
+  var name: String?
+  var detailContent: (T) -> DetailContent
 
-    var name: String?
-    var detailContent: (T) -> DetailContent
-
-    func body(content: Content, info: ResolvedWaypointInfo) -> some View {
-      let pathParameter = info.pathParameter(as: T.self)
-      let activate = pathParameter != nil && rootDetailWaypointContent == nil && info.active
-      return
-        content
-        .environment(
-          \.rootDetailWaypointContent,
-          activate
-            ? RootDetailWaypointContent(waypoint: info.nextWaypoint, content: detailContent(pathParameter!).waypoint(with: info.nextWaypoint))
-            : rootDetailWaypointContent
-        )
-    }
+  func body(content: Content, info: ResolvedWaypointInfo) -> some View {
+    let pathParameter = info.pathParameter(as: T.self)
+    let activate = pathParameter != nil && rootDetailWaypointContent == nil && info.active
+    return
+      content
+      .environment(
+        \.rootDetailWaypointContent,
+        activate
+          ? RootDetailWaypointContent(waypoint: info.nextWaypoint, content: detailContent(pathParameter!).waypoint(with: info.nextWaypoint))
+          : rootDetailWaypointContent
+      )
   }
+}
 
-  extension View {
+extension View {
 
-    /// Create a detail waypoint that accepts a parameter.
-    /// 
-    /// - Parameters:
-    ///   - name: The name of the route.
-    ///   - content: The content of the route.
-    /// - Returns: The view.
-    public func detailItem<T, Content>(_ name: String? = nil, @ViewBuilder content: @escaping (T) -> Content) -> some View
-    where T: LosslessStringConvertible & Equatable, Content: View {
-      self.modifier(DynamicRootDetailWaypointViewModifier(name: name, detailContent: content))
-    }
+  /// Create a detail waypoint that accepts a parameter.
+  /// 
+  /// - Parameters:
+  ///   - name: The name of the route.
+  ///   - content: The content of the route.
+  /// - Returns: The view.
+  public func detailItem<T, Content>(_ name: String? = nil, @ViewBuilder content: @escaping (T) -> Content) -> some View
+  where T: LosslessStringConvertible & Equatable, Content: View {
+    self.modifier(DynamicRootDetailWaypointViewModifier(name: name, detailContent: content))
   }
-
-#endif
+}
